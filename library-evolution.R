612a8bd554bcd4af13f6664923ba39bd34347b23
Anonymous Marmot <anonymous@marmot.com>
2020-01-16 22:59:06 +0100

    Fixed weird sorting error

--- library-evolution.R
+++ library-evolution.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(reshape2)
+library(readr)
+library(stringr)
+library(forcats)
 
 groupId <- "org.apache.felix"
 artifactId <- "org.apache.felix.framework"
@@ -13,15 +16,25 @@ artifactQuery <- paste("[maven.groupId]=\"", groupId, "\" && [maven.artifactId]=
 artifactData <- marmotR::search(query = artifactQuery, limit = 300)
 artifactDF <- jsonlite::flatten(as.data.frame(artifactData$hits))
 
-plotdata <- artifactDF %>% separate(metadata.version, remove = FALSE,  into = c("metadata.version.major", "metadata.version.minor", "metadata.version.patch"))
+plotdata <- artifactDF %>% separate(metadata.version, remove = FALSE,  into = c("metadata.version.major", "metadata.version.minor", "metadata.version.patch")) %>% type_convert()
 plotdata <- plotdata %>% arrange(metadata.version.major, metadata.version.minor, metadata.version.patch)
 
 
 selectedData <- plotdata %>% select(metadata.version, metricResults.metrics.classversion.6, metricResults.metrics.classversion.5, metricResults.metrics.classversion.1.1)
 selectedData <- selectedData %>% rename(`1.1` = metricResults.metrics.classversion.1.1, 
                                         `5` = metricResults.metrics.classversion.5,
-                                        `6` = metricResults.metrics.classversion.6)
-reshaped <- melt(selectedData, id.vars="metadata.version")  %>% separate(metadata.version, remove = FALSE,  into = c("metadata.version.major", "metadata.version.minor", "metadata.version.patch"))
+                                        `6` = metricResults.metrics.classversion.6) 
+
+reshaped <- melt(selectedData, id.vars="metadata.version")  %>% 
+  separate(metadata.version, remove = FALSE,  into = c("metadata.version.major", "metadata.version.minor", "metadata.version.patch"))  %>% 
+  type_convert() %>%
+  arrange(metadata.version.major, metadata.version.minor, metadata.version.patch)  %>%
+  mutate(safeversion = paste(str_pad(metadata.version.major, 5, pad="0"),
+                             str_pad(metadata.version.minor, 5, pad="0"),
+                             str_pad(metadata.version.patch, 5, pad="0"), sep=".")) %>%
+  mutate(metadata.version = fct_reorder(metadata.version, safeversion))
+
+
 
 classVersionByVersion <- ggplot(reshaped, aes(x = metadata.version, y = value, fill=variable)) +
   geom_col(na.rm = TRUE) + 
